4749ad4db631f7687dcaa425da98ec215904c2e1
Ajay Paliwal <ajaypaliwal.cto@outlook.com>
2019-06-24 12:06:35 +0530

    test
    test

--- app/app.js
+++ app/app.js
@@ -5,77 +5,77 @@ app.config(function ($routeProvider) {
 
     $routeProvider.when("/home", {
         controller: "homeController",
-        templateUrl: "/app/views/home.html"
+        templateUrl: "./app/views/home.html"
     });
 
     $routeProvider.when("/login", {
         controller: "loginController",
-        templateUrl: "/app/views/login.html"
+        templateUrl: "./app/views/login.html"
     });
 
     $routeProvider.when("/signup", {
         controller: "signupController",
-        templateUrl: "/app/views/signup.html"
+        templateUrl: "./app/views/signup.html"
     });
 
     $routeProvider.when("/orders", {
         controller: "ordersController",
-        templateUrl: "/app/views/orders.html"
+        templateUrl: "./app/views/orders.html"
     });
 
     $routeProvider.when("/refresh", {
         controller: "refreshController",
-        templateUrl: "/app/views/refresh.html"
+        templateUrl: "./app/views/refresh.html"
     });
 
     $routeProvider.when("/tokens", {
         controller: "tokensManagerController",
-        templateUrl: "/app/views/tokens.html"
+        templateUrl: "./app/views/tokens.html"
     });
 
     $routeProvider.when("/associate", {
         controller: "associateController",
-        templateUrl: "/app/views/associate.html"
+        templateUrl: "./app/views/associate.html"
     });    
 
     $routeProvider.when("/project", {
         controller: "projectController",
-        templateUrl: "/app/views/project.html"
+        templateUrl: "./app/views/project.html"
     });
 
     $routeProvider.when("/assets", {
         controller: "AssetsController",
-        templateUrl: "/app/views/Asset.html"
+        templateUrl: "./app/views/Asset.html"
     });
 
     $routeProvider.when("/category", {
         controller: "CategoryController",
-        templateUrl: "/app/views/Category.html"
+        templateUrl: "./app/views/Category.html"
     });
 
     $routeProvider.when("/expense", {
         controller: "ExpenseController",
-        templateUrl: "/app/views/expense.html"
+        templateUrl: "./app/views/expense.html"
     });
 
     $routeProvider.when("/incoming", {
         controller: "incomingController",
-        templateUrl: "/app/views/incoming.html"
+        templateUrl: "./app/views/incoming.html"
     });
 
     $routeProvider.when("/supplier", {
         controller: "SupplierController",
-        templateUrl: "/app/views/Supplier.html"
+        templateUrl: "./app/views/Supplier.html"
     });
 
     $routeProvider.when("/manufacturer", {
         controller: "ManufacturerController",
-        templateUrl: "/app/views/Manufacturer.html"
+        templateUrl: "./app/views/Manufacturer.html"
     });
 
     $routeProvider.when("/reports", {
         controller: "ReportsController",
-        templateUrl: "/app/views/Reports.html"
+        templateUrl: "./app/views/Reports.html"
     });
 
     $routeProvider.otherwise({ redirectTo: "/login" });
